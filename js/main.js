b7f5af5b69a84546afe72a2b48c61c4dcca7b425
Ed Clements <ed@bookingbug.com>
2014-10-08 22:59:27 +0100

    Add Lauren WK

--- js/main.js
+++ js/main.js
@@ -120,6 +120,7 @@ myApp.service('invitees', ['$window', function(win) {
     {id: 90,  group: 43, ceremony: true,  pie: false, first_name: 'Ruby',        last_name: 'Valenciano'},
     {id: 91,  group: 44, ceremony: true,  pie: true,  first_name: 'Terry',       last_name: 'Curtis'},
     {id: 92,  group: 45, ceremony: true,  pie: true,  first_name: 'Thomas',      last_name: 'Campbell'},
+    {id: 93,  group: 46, ceremony: true,  pie: true,  first_name: 'Lauren',      last_name: 'Wilson-Kelly'},
     {id: 998, group: 99, ceremony: true,  pie: true,  first_name: 'Edward',      last_name: 'Clements',    nick: 'Ed'},
     {id: 999, group: 99, ceremony: true,  pie: true,  first_name: 'Victoria',    last_name: 'Jesper',      nick: 'Vic'}
   ];
